08168c32c4a849fc56e6752ed3dc14536ca6485e
Fabian Hernandez <fabian.hr8@gmail.com>
2021-07-19 17:10:50 -0500

    Added plugin to the site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,11 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "gatsbyWebsite1",
+    title: "gatsbyWebsite",
   },
-  plugins: [],
+  plugins: [
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp'
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import Layout from '../components/layout';
-import { title } from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 // markup
 const IndexPage = () => {
   return (
     <Layout pageTitle='Home Page'>
       <p>This is an example made from the Gatsby tutorial</p>
+      <StaticImage 
+        alt='A cat in a sofa'
+        // src='https://live-production.wcms.abc-cdn.net.au/7a37d3659e19ded1fec899317b8c5c26?impolicy=wcms_crop_resize&cropH=1373&cropW=2441&xPos=0&yPos=0&width=862&height=485'
+        src='../images/matilda.jpg'
+      />
     </Layout>
   )
 }
