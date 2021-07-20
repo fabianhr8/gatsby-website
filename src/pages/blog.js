e75b95a6cb58c97445c49d578e1ab0100b499660
Fabian Hernandez <fabian.hr8@gmail.com>
2021-07-20 10:02:05 -0500

    Finished tutorial

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,33 +1,39 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
 import Layout from '../components/layout';
 
-const BlogPage = () => {
-    
-    const query =  useStaticQuery(graphql`
-        query {
-            allFile {
-                nodes {
-                    name
-                }
-            }
+const BlogPage = ({ data }) => {
+
+    const query = useStaticQuery(graphql`
+    query getPosts {
+      allMdx {
+        nodes {
+          frontmatter {
+            title
+            date(formatString: "MMMM D, YYYY")
+          }
+          id
+          body
         }
-    `);
-    console.log(query)
+      }
+    }
+  `);
 
     return (
       <Layout pageTitle="My Blog Posts">
-        <ul>
         {
-            query.allFile.nodes.map(node => (
-                <li key={node.name}>
-                    {node.name}
-                </li>
-            ))
+          query.allMdx.nodes.map((node) => (
+            <article key={node.id}>
+              <h2>{node.frontmatter.title}</h2>
+              <p>Posted: {node.frontmatter.date}</p>
+              <MDXRenderer>
+                {node.body}
+              </MDXRenderer>
+            </article>
+          ))
         }
-        </ul>
       </Layout>
     )
-}
-
-export default BlogPage;
+  }
+  export default BlogPage;
